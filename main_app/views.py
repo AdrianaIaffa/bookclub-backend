c9957586589126580a08bb2553fe3417f11b4b8d
AdrianaIaffa <adrianaiaffa@hotmail.com>
2023-11-23 15:14:06 +0000

    fix logout issues

--- main_app/views.py
+++ main_app/views.py
@@ -131,12 +131,12 @@ class LogoutView(APIView):
     permission_classes = (IsAuthenticated,)
     def post(self, request):
         try:
-            refresh_token = request.data["refresh_token"]
+            refresh_token = request.body["refresh_token"]
             token = RefreshToken(refresh_token)
             token.blacklist()
             return Response(status=status.HTTP_205_RESET_CONTENT)
         except Exception as e:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
 class UserRegistrationView(viewsets.ModelViewSet):
     queryset = User.objects.all()
